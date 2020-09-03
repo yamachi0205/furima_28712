9160f9b24c333769a8417a2ee75463ef57489aad
yamachi0205 <yama.513421@gmail.com>
2020-09-03 16:57:33 +0900

    users テーブル修正

--- db/migrate/20200901062443_devise_create_users.rb
+++ db/migrate/20200901062443_devise_create_users.rb
@@ -12,7 +12,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name_reading,   null: false, default: ""
       t.string :nickname,             null: false, default: ""
       t.string :password,             null: false, default: ""
-      t.date   :dirthday,             null: false
+      t.date   :birthday,             null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_062443) do
     t.string "first_name_reading", default: "", null: false
     t.string "nickname", default: "", null: false
     t.string "password", default: "", null: false
-    t.date "dirthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
