8908a50814c415d765cd07aafd483451b924d4d2
yamachi0205 <yama.513421@gmail.com>
2020-09-08 11:22:33 +0900

    ヘッダーにリンクを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    User.create(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:family_name, :first_name, :family_name_reading, :first_name_reading, :nickname, :birthday, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    validates :family_name, format:{with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format:{with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :family_name_reading, format:{with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_reading, format:{with: /\A[ァ-ヶー－]+\z/}
+    validates :nickname
+    validates :password
+    validates :birthbay
+  end
   
 end
