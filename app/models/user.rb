3cbf8fb85f30123365ae88ed7244efba9386c5fb
yamachi0205 <yama.513421@gmail.com>
2020-09-08 16:06:03 +0900

    パスワードに英数字混合のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VILID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
   with_options presence: true do
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :nickname
-    validates :password
+    validates :password, format: { with: VILID_PASSWORD_REGEX}
     validates :birthday
   end
 end
