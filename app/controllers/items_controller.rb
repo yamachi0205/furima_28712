78c4555598c6cd1557ebc9bc9bc1a17ccf5b8620
yamachi0205 <yama.513421@gmail.com>
2020-09-10 20:58:34 +0900

    エラーハンドリング追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,10 +21,13 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
-
+  
   def new
     @item = Item.new
   end
