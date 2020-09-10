fc934ea9172383a8097c23066872b8600fe68fc4
yamachi0205 <yama.513421@gmail.com>
2020-09-10 20:56:47 +0900

    出品時の画面移動変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to item_path
+      redirect_to root_path
     else
       render :new
     end
