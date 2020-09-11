6349260f13a318f96b4397f3a78694bcdfd58576
yamachi0205 <yama.513421@gmail.com>
2020-09-11 11:35:53 +0900

    order マイグレーション修正

--- db/migrate/20200911021809_create_orders.rb
+++ db/migrate/20200911021809_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.integer :user_id
-      t.integer :item_id
+      t.integer :user_id,  foring_key: true
+      t.integer :item_id,  foring_key: true
       t.timestamps
     end
   end
