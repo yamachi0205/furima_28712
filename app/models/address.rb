2c7dbd4347261684699eca465a140ee6bfc4b722
yamachi0205 <yama.513421@gmail.com>
2020-09-11 12:13:41 +0900

    orderバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+
+  with_options presence: true do
+    validates :postal_code, form: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture
+    validates :city
+    validates :house_number
+    validates :phone_number,  form: { with: /\A\d{11}\z/}
+  end
 end
